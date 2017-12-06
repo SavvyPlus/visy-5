4a5185d38bbb0bdd0fbad46e151595558fcdcaf9
Kev Jackson <foamdino@gmail.com>
2017-12-06 19:41:13 +0000

    add lambda handler

--- total_cleared.py
+++ total_cleared.py
@@ -127,3 +127,16 @@ if __name__ == '__main__':
     create_s3_key(dt)
 
     process_file("test-total-cleared-file")
+
+def lambda_handler(event, context):
+    # call create_s3_key with a value
+    in_date = "2017/10/26 20:40:00"
+    dt = datetime.strptime(in_date, '%Y/%m/%d %H:%M:%S')
+    create_s3_key(dt)
+
+    print("Object added to: [%s]" % (event['Records'][0]['s3']['bucket']['name'],))
+    filename = event['Records'][0]['s3']['object']['key'].split('/')[-1]
+    print(filename)
+
+    #process_file("test-file")
+    process_file(filename)
